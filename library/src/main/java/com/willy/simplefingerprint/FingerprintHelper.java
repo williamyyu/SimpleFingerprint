6241c8890d0588ee2946f3ae6d34a95790a6783a
WillyYu <ome450901@gmail.com>
2017-06-05 15:24:32 +0800

    Check if key exist before create it

--- library/src/main/java/com/willy/simplefingerprint/FingerprintHelper.java
+++ library/src/main/java/com/willy/simplefingerprint/FingerprintHelper.java
@@ -38,7 +38,6 @@ class FingerprintHelper extends FingerprintManager.AuthenticationCallback {
     private FingerprintManager mFingerprintManager;
 
     private KeyStore mKeyStore;
-    private KeyGenerator mKeyGenerator;
 
     private Cipher mCipher;
     private CancellationSignal mCancellationSignal;
@@ -57,14 +56,63 @@ class FingerprintHelper extends FingerprintManager.AuthenticationCallback {
             LogUtils.log("Failed to get an instance of KeyStore", e);
         }
 
+        createKey(KEY_NAME, true);
+    }
+
+    /**
+     * Creates a symmetric key in the Android Key Store which can only be used after the user has
+     * authenticated with fingerprint.
+     *
+     * @param keyName                          the name of the key to be created
+     * @param invalidatedByBiometricEnrollment if {@code false} is passed, the created key will not
+     *                                         be invalidated even if a new fingerprint is enrolled.
+     *                                         The default value is {@code true}, so passing
+     *                                         {@code true} doesn't change the behavior
+     *                                         (the key will be invalidated if a new fingerprint is
+     *                                         enrolled.). Note that this parameter is only valid if
+     *                                         the app works on Android N developer preview.
+     */
+    private void createKey(String keyName, boolean invalidatedByBiometricEnrollment) {
+        // The enrolling flow for fingerprint. This is where you ask the user to set up fingerprint
+        // for your flow. Use of keys is necessary if you need to know if the set of
+        // enrolled fingerprints has changed.
         try {
-            mKeyGenerator = KeyGenerator
+            mKeyStore.load(null);
+            // Set the alias of the entry in Android KeyStore where the key will appear
+            // and the constrains (purposes) in the constructor of the Builder
+
+            if (mKeyStore.containsAlias(keyName)) {
+                return;
+            }
+
+            KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(
+                    keyName,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    // Require the user to authenticate with a fingerprint to authorize every use of the key
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
+
+            /*
+                Sets whether this key should be invalidated on fingerprint enrollment.
+                Only available in API level >= 24
+                Default is true.
+             */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                builder.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment);
+            }
+
+            KeyGenerator keyGenerator = KeyGenerator
                     .getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
-        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            keyGenerator.init(builder.build());
+            keyGenerator.generateKey();
+
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | CertificateException | IOException | KeyStoreException e) {
+            LogUtils.log("Failed to createKey", e);
+        } catch (NoSuchProviderException e) {
             LogUtils.log("Failed to get an instance of KeyGenerator", e);
         }
-
-        createKey(KEY_NAME, true);
     }
 
     /**
@@ -94,6 +142,17 @@ class FingerprintHelper extends FingerprintManager.AuthenticationCallback {
                 | NoSuchAlgorithmException | InvalidKeyException e) {
             LogUtils.log("Failed to init Cipher", e);
             return false;
+        }
+    }
+
+    private Cipher createCipher() {
+        try {
+            return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            LogUtils.log("Failed to get an instance of Cipher", e);
+            return null;
         }
     }
 
@@ -148,64 +207,6 @@ class FingerprintHelper extends FingerprintManager.AuthenticationCallback {
     boolean hasEnrolledFingerprints() {
         // noinspection ResourceType
         return mFingerprintManager.hasEnrolledFingerprints();
-    }
-
-    /**
-     * Creates a symmetric key in the Android Key Store which can only be used after the user has
-     * authenticated with fingerprint.
-     *
-     * @param keyName                          the name of the key to be created
-     * @param invalidatedByBiometricEnrollment if {@code false} is passed, the created key will not
-     *                                         be invalidated even if a new fingerprint is enrolled.
-     *                                         The default value is {@code true}, so passing
-     *                                         {@code true} doesn't change the behavior
-     *                                         (the key will be invalidated if a new fingerprint is
-     *                                         enrolled.). Note that this parameter is only valid if
-     *                                         the app works on Android N developer preview.
-     */
-    private void createKey(String keyName, boolean invalidatedByBiometricEnrollment) {
-        // The enrolling flow for fingerprint. This is where you ask the user to set up fingerprint
-        // for your flow. Use of keys is necessary if you need to know if the set of
-        // enrolled fingerprints has changed.
-        try {
-            mKeyStore.load(null);
-            // Set the alias of the entry in Android KeyStore where the key will appear
-            // and the constrains (purposes) in the constructor of the Builder
-
-            KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(
-                    keyName,
-                    KeyProperties.PURPOSE_ENCRYPT |
-                            KeyProperties.PURPOSE_DECRYPT)
-                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                    // Require the user to authenticate with a fingerprint to authorize every use of the key
-                    .setUserAuthenticationRequired(true)
-                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
-
-            /*
-                Sets whether this key should be invalidated on fingerprint enrollment.
-                Only available in API level >= 24
-                Default is true.
-             */
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                builder.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment);
-            }
-            mKeyGenerator.init(builder.build());
-            mKeyGenerator.generateKey();
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException
-                | CertificateException | IOException e) {
-            LogUtils.log("Failed to createKey", e);
-        }
-    }
-
-    private Cipher createCipher() {
-        try {
-            return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
-                    + KeyProperties.BLOCK_MODE_CBC + "/"
-                    + KeyProperties.ENCRYPTION_PADDING_PKCS7);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            LogUtils.log("Failed to get an instance of Cipher", e);
-            return null;
-        }
     }
 
     @Override
